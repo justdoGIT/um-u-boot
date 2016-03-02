25d356544c6fe326db198d26083bfd93549bf2c3
Olliver Schinagl <o.schinagl@ultimaker.com>
2016-03-02 10:14:34 +0100

    Add detection for a cross compiler
    This patch detects the cross compiler to be used and allows it to
    overridden as the assumed default may simply not be available or named
    differently.
    
    EM-253
    
    Signed-off-by: Olliver Schinagl <o.schinagl@ultimaker.com>

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Check for a valid cross compiler. When unset, the kernel tries to build itself
+# using arm-none-eabi-gcc, so we need to ensure it exists. Because printenv and
+# which can cause bash -e to exit, so run this before setting this up.
+CROSS_COMPILE=$(printenv CROSS_COMPILE)
+if [ ${CROSS_COMPILE+x} ]; then
+    _CROSS_COMPILE=`which arm-none-eabi-gcc`
+    if [ -z ${_CROSS_COMPILE} ]; then
+        _CROSS_COMPILE=`which arm-linux-gnueabihf-gcc`
+        if [ ${_CROSS_COMPILE} ]; then
+            CROSS_COMPILE="arm-linux-gnueabihf-"
+            export CROSS_COMPILE=${CROSS_COMPILE}
+        else
+            echo "No suiteable cross-compiler found."
+            echo "One can be set explicitly via the environment variable CROSS_COMPILE='arm-linux-gnueabihf-' for example."
+	    exit
+        fi
+    fi
+fi
+
 set -e
 set -u
 
@@ -11,7 +30,6 @@ if [ -z ${RELEASE_VERSION+x} ]; then
 	RELEASE_VERSION=9999.99.99
 fi
 BUILDCONFIG="opinicus_v1"
-CROSS_COMPILE="arm-none-eabi-"
 
 #Build the actual bootloader
 ARCH=arm CROSS_COMPILE="${CROSS_COMPILE}" make "${BUILDCONFIG}_defconfig"
